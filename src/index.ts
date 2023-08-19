019d101ca98bdfa962435cf776f1fe3346d4e151
0xshoegazer <0xshoegazer@gmail.com>
2023-08-19 17:26:21 -0400

    full package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import './sdk-core';
+import './v3-sdk';
