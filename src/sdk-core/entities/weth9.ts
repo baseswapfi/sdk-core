acff0d1392e5f2904c44d545057be5a285d020fa
0xshoegazer <0xshoegazer@gmail.com>
2023-10-22 09:59:50 -0400

    WETH9

--- src/sdk-core/entities/weth9.ts
+++ src/sdk-core/entities/weth9.ts
@@ -5,4 +5,6 @@ import { Token } from './token';
  */
 export const WETH9: { [chainId: number]: Token } = {
   [8453]: new Token(8453, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
+  [534352]: new Token(534352, '0x5300000000000000000000000000000000000004', 18, 'WETH', 'Wrapped Ether'),
+  [534351]: new Token(534351, '0x5300000000000000000000000000000000000004', 18, 'WETH', 'Wrapped Ether'),
 };
