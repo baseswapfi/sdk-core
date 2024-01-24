8e7a14e512d6535245037e99fb9b1f5cfcb24dfc
0xshoegazer <0xshoegazer@gmail.com>
2024-01-24 18:10:45 -0500

    frax testnest chain

--- src/sdk-core/chains.ts
+++ src/sdk-core/chains.ts
@@ -2,6 +2,7 @@ export enum ChainId {
   ARBITRUM = 42161,
   BASE_GOERLI = 84531,
   BASE = 8453,
+  FRAX_TESTNET = 2522,
   OPTIMISM = 10,
   SCROLL = 534352,
   SCROLL_SEPOLIA = 534351,
@@ -15,6 +16,9 @@ export const SUPPORTED_CHAINS = [
   ChainId.ARBITRUM,
 ] as const;
 export type SupportedChainsType = typeof SUPPORTED_CHAINS[number];
+
+export const SUPPORTED_TESTNET_CHAINS = [ChainId.BASE_GOERLI, ChainId.SCROLL_SEPOLIA, ChainId.FRAX_TESTNET] as const;
+export type SupportedTestnetChainsType = typeof SUPPORTED_TESTNET_CHAINS[number];
 
 export enum NativeCurrencyName {
   // Strings match input for CLI
