3a6c68bf1ebf51a9996e8a268074b99bd668fbc8
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2024-11-15 15:03:48 -0500

    weth9 for op

--- src/sdk-core/entities/weth9.ts
+++ src/sdk-core/entities/weth9.ts
@@ -9,6 +9,7 @@ export const WETH9: { [chainId: number]: Token } = {
   [919]: new Token(919, '0xeb72756ee12309Eae82a0deb9787e69f5b62949c', 18, 'WETH', 'Wrapped Ether'),
   [1946]: new Token(1946, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
   [42161]: new Token(42161, '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', 18, 'WETH', 'Wrapped Ether'),
+  [10]: new Token(10, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
 
 
 };
